1280a7870945848837401a80ec7171f322a4a6b5
drgmk <gkennedy@ast.cam.ac.uk>
2017-08-02 16:48:30 +0100

    add basic config

--- classifier/config.py
+++ classifier/config.py
@@ -1,4 +1,18 @@
-training_dir = '/Users/grant/astro/projects/classifier/classifier/training/'
+import os
+# where the module files are
+here = os.path.dirname(os.path.realpath(__file__))
 
-sdb_user = 'drgmk'
-sdb_pass = 'kmgrd'
+# fill in to get rawphot files from sdb, only needed for getting new
+# files for training
+sdb_user = ''
+sdb_pass = ''
+
+# where in-module training stuff is
+training_dir = here + '/../training/'
+
+# where external (i.e. not with this module) spectra are
+spectra = here + '/../../spectra/'
+
+# median filters to apply when reading spectra for classifying/labelling
+median_classifying = 30
+median_labelling = 3
